903005bcfc68389ba5661ae55d5188c91cbc4f8d
Bryan <brayan-rodas@hotmail.com>
2022-08-03 09:57:41 -0500

    primer commit

---
+++ src/components/MyLabel.tsx
@@ -0,0 +1,43 @@
+import "./myLabel.css";
+
+export interface MyLabelProps {
+  /**
+   * Is this the label that will be display
+   */
+  label: string;
+  /**
+   * Is this the size of the label
+   */
+  size: "normal" | "h1" | "h2" | "h3";
+  /**
+   * Is this the color of the label
+   */
+  color: "primary" | "secondary" | "tertiary";
+  /**
+   * All letters are in capital
+   */
+  allCaps: boolean;
+  /**
+   * All letters are in capital
+   */
+  fontColor: string;
+}
+
+const MyLabel = ({
+  label = "no label",
+  size = "normal",
+  color = "primary",
+  allCaps = false,
+  fontColor,
+}: MyLabelProps) => {
+  return (
+    <span
+      className={`label ${size} text-${color}`}
+      style={{ color: fontColor }}
+    >
+      {allCaps ? label.toUpperCase() : label}
+    </span>
+  );
+};
+
+export default MyLabel;
---
+++ src/stories/components/MyLabel.stories.tsx
@@ -0,0 +1,27 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import MyLabel from "../../components/MyLabel";
+
+export default {
+  title: "UI/MyLabel",
+  component: MyLabel,
+  argTypes: {
+    size: { control: "select" },
+    color: { control: "select" },
+    fontColor: { control: "color" },
+  },
+} as ComponentMeta<typeof MyLabel>;
+
+const Template: ComponentStory<typeof MyLabel> = (args) => (
+  <MyLabel {...args} />
+);
+
+export const Basic = Template.bind({});
+Basic.args = { size: "normal" };
+export const AllCaps = Template.bind({});
+AllCaps.args = { size: "normal", allCaps: true, color: "primary" };
+export const Seconary = Template.bind({});
+Seconary.args = { size: "normal", color: "secondary" };
+export const Tertiary = Template.bind({});
+Tertiary.args = { size: "normal", color: "tertiary" };
+export const CustomFontColor = Template.bind({});
+CustomFontColor.args = { size: "h1", fontColor: "#f017ac" };
